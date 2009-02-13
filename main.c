24ca5cb7b7ca6d2c7ebef49d8efd959bf81c0d5c
jason <jason@jmarcell.com>
2009-02-13 17:06:53 -0500

    indent style

--- main.c
+++ main.c
@@ -5,44 +5,44 @@ int func4();
 
 main()
 {
-	short mat[3][3],i,j;
+  short mat[3][3],i,j;
 	
-	for(i = 0 ; i < 3 ; i++)
-		for(j = 0 ; j < 3 ; j++)
-		{
-			mat[i][j] = i*10 + j;
-		}
+  for(i = 0 ; i < 3 ; i++)
+    for(j = 0 ; j < 3 ; j++)
+      {
+	mat[i][j] = i*10 + j;
+      }
 	
-	printf(" Initialized data to: ");
-	for(i = 0 ; i < 3 ; i++)
+  printf(" Initialized data to: ");
+  for(i = 0 ; i < 3 ; i++)
+    {
+      printf("\n");
+      for(j = 0 ; j < 3 ; j++)
 	{
-		printf("\n");
-		for(j = 0 ; j < 3 ; j++)
-		{
-			printf("%5.2d", mat[i][j]);
-		}
+	  printf("%5.2d", mat[i][j]);
 	}
-	printf("\n");
+    }
+  printf("\n");
 	
-	func4(mat);
+  func4(mat);
 }
 
 int func4(short **mat) {
-	short    i, j, *index[3];
+  short    i, j, *index[3];
 	
-	for (i = 0 ; i < 3 ; i++)
-		index[i] = (short *)mat + 3*i;
+  for (i = 0 ; i < 3 ; i++)
+    index[i] = (short *)mat + 3*i;
 	
-	printf(" Declare as double-pointer, use auxiliary pointer array: ");
-	for(i = 0 ; i < 3 ; i++)
+  printf(" Declare as double-pointer, use auxiliary pointer array: ");
+  for(i = 0 ; i < 3 ; i++)
+    {
+      printf("\n");
+      for(j = 0 ; j < 3 ; j++)
 	{
-		printf("\n");
-		for(j = 0 ; j < 3 ; j++)
-		{
-			printf("%5.2d", index[i][j]);
-		}
+	  printf("%5.2d", index[i][j]);
 	}
-	printf("\n");
+    }
+  printf("\n");
 	
-	return;
+  return;
 }
