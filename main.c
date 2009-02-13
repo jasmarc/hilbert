598e8eb528f259a93d4c52ff5045f4514f029fc5
jason <jason@jmarcell.com>
2009-02-13 18:58:51 -0500

    fixing array print routine

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define CWID 4
-#define CHGT 3
+#define CHGT 6
 
 int func4();
 
 main()
 {
-    short mat[CHGT][CWID], i, j;
+    short mat[CWID][CHGT], i, j;
 
     for (j = 0; j < CHGT; j++)
 	for (i = 0; i < CWID; i++) {
-	    mat[j][i] = j * CWID + i;
+	    mat[i][j] = j * CWID + i;
 	}
 
     printf(" Initialized data to: ");
     for (j = 0; j < CHGT; j++) {
 	printf("\n");
 	for (i = 0; i < CWID; i++) {
-	    printf("%5.2d", mat[j][i]);
+	    printf("%5.2d", mat[i][j]);
 	}
     }
     printf("\n");
@@ -28,16 +28,16 @@ main()
 
 int func4(short **mat, int width, int height)
 {
-    short i, j, *index[width];
+    short i, j, *index[height];
 
-    for (i = 0; i < height; i++)
-	index[i] = (short *) mat + width * i;
+    for (i = 0; i < width; i++)
+	index[i] = (short *) mat + height * i;
 
     printf(" Declare as double-pointer, use auxiliary pointer array: ");
     for (j = 0; j < height; j++) {
 	printf("\n");
 	for (i = 0; i < width; i++) {
-	    printf("%5.2d", index[j][i]);
+	    printf("%5.2d", index[i][j]);
 	}
     }
     printf("\n");
