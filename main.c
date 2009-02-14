dddb7181d1054d53d565a6f8075efcd7045d4435
Jason Marcell <jason@jason-marcells-macbook.local>
2009-02-13 19:58:45 -0500

    initial array work

--- main.c
+++ main.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main (int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!!!!\n");
-    return 0;
+int func4();
+
+main()
+{
+	short mat[3][3],i,j;
+	
+	for(i = 0 ; i < 3 ; i++)
+		for(j = 0 ; j < 3 ; j++)
+		{
+			mat[i][j] = i*10 + j;
+		}
+	
+	printf(" Initialized data to: ");
+	for(i = 0 ; i < 3 ; i++)
+	{
+		printf("\n");
+		for(j = 0 ; j < 3 ; j++)
+		{
+			printf("%5.2d", mat[i][j]);
+		}
+	}
+	printf("\n");
+	
+	func4(mat);
+}
+
+int func4(short **mat) {
+	short    i, j, *index[3];
+	
+	for (i = 0 ; i < 3 ; i++)
+		index[i] = (short *)mat + 3*i;
+	
+	printf(" Declare as double-pointer, use auxiliary pointer array: ");
+	for(i = 0 ; i < 3 ; i++)
+	{
+		printf("\n");
+		for(j = 0 ; j < 3 ; j++)
+		{
+			printf("%5.2d", index[i][j]);
+		}
+	}
+	printf("\n");
+	
+	return;
 }
